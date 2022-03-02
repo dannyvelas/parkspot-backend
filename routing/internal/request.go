2e78100da11687d4a4ca08ce0f7e2460abfad2f1
Daniel Velasquez <danivelas4@gmail.com>
2022-03-01 19:39:14 -0500

    routing/internal/request: small conditional syntax changes

--- routing/internal/request.go
+++ routing/internal/request.go
@@ -5,23 +5,20 @@ import "strconv"
 func ToUint(value string) uint {
 	if value == "" {
 		return 0
-	}
-
-	res, err := strconv.ParseUint(value, 10, 32)
-	if err != nil {
+	} else if parsed, err := strconv.ParseUint(value, 10, 32); err != nil {
 		return 0
+	} else {
+		return uint(parsed)
 	}
-
-	return uint(res)
 }
 
 func GetBoundedSizeAndOffset(size, page uint) (boundedSize, offset uint) {
-	if 0 < size && size < maxPageSize {
-		boundedSize = size
-	} else if size > maxPageSize {
+	if size > maxPageSize {
 		boundedSize = maxPageSize
-	} else {
+	} else if size < 0 {
 		boundedSize = defaultPageSize
+	} else {
+		boundedSize = size
 	}
 
 	if page > 1 {
