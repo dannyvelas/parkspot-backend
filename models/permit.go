6041fce8ef889aa77302e52d120eadbe7105e1ef
Daniel Velasquez <danivelas4@gmail.com>
2022-04-17 20:01:00 -0400

    models/permit.go: small changes in json schema tags and function order

--- models/permit.go
+++ models/permit.go
@@ -8,36 +8,16 @@ import (
 
 type Permit struct {
 	Id          int       `json:"id"`
-	ResidentId  string    `json:"resident_id"`
+	ResidentId  string    `json:"residentId"`
 	Car         Car       `json:"car"`
-	StartDate   time.Time `json:"start_date"`
-	EndDate     time.Time `json:"end_date"`
-	RequestTS   int64     `json:"request_ts"`
-	AffectsDays bool      `json:"affects_days"`
-}
-
-func (self Permit) Equal(other Permit) bool {
-	if self.Id != other.Id {
-		return false
-	} else if self.ResidentId != other.ResidentId {
-		return false
-	} else if !cmp.Equal(self.Car, other.Car) {
-		return false
-	} else if !self.StartDate.Equal(other.StartDate) {
-		return false
-	} else if !self.EndDate.Equal(other.EndDate) {
-		return false
-	} else if self.RequestTS != other.RequestTS {
-		return false
-	} else if self.AffectsDays != other.AffectsDays {
-		return false
-	}
-
-	return true
+	StartDate   time.Time `json:"startDate"`
+	EndDate     time.Time `json:"endDate"`
+	RequestTS   int64     `json:"requestTS"`
+	AffectsDays bool      `json:"affectsDays"`
 }
 
 func NewPermit(id int, residentId string, car Car, startString string, endString string, requestTS int64, affectsDays bool) (Permit, error) {
-	dateFormat := "2006-01-02"
+	const dateFormat = "2006-01-02"
 
 	startDate, err := time.ParseInLocation(dateFormat, startString, time.Local)
 	if err != nil {
@@ -58,4 +38,24 @@ func NewPermit(id int, residentId string, car Car, startString string, endString
 		RequestTS:   requestTS,
 		AffectsDays: affectsDays,
 	}, nil
+}
+
+func (self Permit) Equal(other Permit) bool {
+	if self.Id != other.Id {
+		return false
+	} else if self.ResidentId != other.ResidentId {
+		return false
+	} else if !cmp.Equal(self.Car, other.Car) {
+		return false
+	} else if !self.StartDate.Equal(other.StartDate) {
+		return false
+	} else if !self.EndDate.Equal(other.EndDate) {
+		return false
+	} else if self.RequestTS != other.RequestTS {
+		return false
+	} else if self.AffectsDays != other.AffectsDays {
+		return false
+	}
+
+	return true
 }
