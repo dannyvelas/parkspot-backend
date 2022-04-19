02d6f32b440c593c0288acf58a8377d12b6517af
Daniel Velasquez <danivelas4@gmail.com>
2022-04-19 19:42:37 -0400

    storage/convert.go: added startDate and endDate conversion func to time.Times

--- storage/convert.go
+++ storage/convert.go
@@ -1,9 +1,28 @@
 package storage
 
+import (
+	"fmt"
+	"time"
+)
+
 func getBoundedLimit(limit uint) uint {
 	if limit > maxLimit {
 		return maxLimit
 	} else {
 		return limit
 	}
+}
+
+func parseStartEndDate(format, startDate, endDate string) (time.Time, time.Time, error) {
+	startTime, err := time.ParseInLocation(format, startDate, time.Local)
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("Error parsing startDate: %v", err)
+	}
+
+	endTime, err := time.ParseInLocation(format, endDate, time.Local)
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("Error parsing endDate: %v", err)
+	}
+
+	return startTime, endTime, nil
 }
