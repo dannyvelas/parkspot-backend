7d15e91437a3bbe114568e6616caf3269aa0ad02
Daniel Velasquez <danivelas4@gmail.com>
2022-04-19 12:22:29 -0400

    storage/permit_repo.go: started using squirrel for querying permits

--- storage/permit_repo.go
+++ storage/permit_repo.go
@@ -2,43 +2,46 @@ package storage
 
 import (
 	"fmt"
+	"github.com/Masterminds/squirrel"
 	"github.com/dannyvelas/lasvistas_api/models"
 )
 
 type PermitRepo struct {
-	database Database
+	database     Database
+	permitSelect squirrel.SelectBuilder
 }
 
 func NewPermitRepo(database Database) PermitRepo {
-	return PermitRepo{database: database}
+	permitSelect := squirrel.Select(
+		"permit.id AS permit_id",
+		"permit.resident_id",
+		"car.id AS car_id",
+		"car.license_plate",
+		"car.color",
+		"car.make",
+		"car.model",
+		"permit.start_ts",
+		"permit.end_ts",
+		"permit.request_ts",
+		"permit.affects_days",
+	).From("permit").LeftJoin("car ON permit.car_id = car.id")
+
+	return PermitRepo{database: database, permitSelect: permitSelect}
 }
 
 func (permitRepo PermitRepo) GetActive(limit, offset uint) ([]models.Permit, error) {
-	const query = `
-    SELECT
-      permit.id AS permit_id,
-      permit.resident_id,
-      car.id AS car_id,
-      car.license_plate,
-      car.color,
-      car.make,
-      car.model,
-      permit.start_ts,
-      permit.end_ts,
-      permit.request_ts,
-      permit.affects_days
-    FROM permit
-    LEFT JOIN car ON
-      permit.car_id = car.id 
-    WHERE
-      permit.start_ts <= extract(epoch from now())
-      AND permit.end_ts >= extract(epoch from now())
-    LIMIT $1
-    OFFSET $2
-  `
+	query, _, err := permitRepo.permitSelect.
+		Where("permit.start_ts <= extract(epoch from now())").
+		Where("permit.end_ts >= extract(epoch from now())").
+		Limit(uint64(getBoundedLimit(limit))).
+		Offset(uint64(offset)).
+		ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("permit_repo.GetActive: %w: %v", ErrDatabaseQuery, err)
+	}
 
 	permits := permitSlice{}
-	err := permitRepo.database.driver.Select(&permits, query, getBoundedLimit(limit), offset)
+	err = permitRepo.database.driver.Select(&permits, query)
 	if err != nil {
 		return nil, fmt.Errorf("permit_repo.GetActive: %w: %v", ErrDatabaseQuery, err)
 	}
@@ -47,28 +50,16 @@ func (permitRepo PermitRepo) GetActive(limit, offset uint) ([]models.Permit, err
 }
 
 func (permitRepo PermitRepo) GetAll(limit, offset uint) ([]models.Permit, error) {
-	const query = `
-    SELECT
-      permit.id AS permit_id,
-      permit.resident_id,
-      car.id AS car_id,
-      car.license_plate,
-      car.color,
-      car.make,
-      car.model,
-      permit.start_ts,
-      permit.end_ts,
-      permit.request_ts,
-      permit.affects_days
-    FROM permit
-    LEFT JOIN car ON
-      permit.car_id = car.id
-    LIMIT $1
-    OFFSET $2
-  `
+	query, _, err := permitRepo.permitSelect.
+		Limit(uint64(getBoundedLimit(limit))).
+		Offset(uint64(offset)).
+		ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("permit_repo.GetAll: %w: %v", ErrDatabaseQuery, err)
+	}
 
 	permits := permitSlice{}
-	err := permitRepo.database.driver.Select(&permits, query, getBoundedLimit(limit), offset)
+	err = permitRepo.database.driver.Select(&permits, query)
 	if err != nil {
 		return nil, fmt.Errorf("permit_repo.GetAll: %w: %v", ErrDatabaseQuery, err)
 	}
