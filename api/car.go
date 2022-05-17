5f05c9a36ec5dd77f6fb03b0b0d9ca2eaf3aa712
Daniel Velasquez <danivelas4@gmail.com>
2022-05-16 20:34:01 -0400

    api/car: added `editCarReq` and moved color, make, and model validation to helper func

--- api/car.go
+++ api/car.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+// new car request
+
 type newCarReq struct {
 	LicensePlate string `json:"licensePlate"`
 	Color        string `json:"color"`
@@ -43,22 +45,16 @@ func (newCarReq newCarReq) invalidFields() error {
 	if !regexp.MustCompile("^[A-Za-z0-9]+$").MatchString(newCarReq.LicensePlate) {
 		errors = append(errors, "licensePlate can only be letters or numbers")
 	}
-
 	if len(newCarReq.LicensePlate) > 8 {
 		errors = append(errors, "licensePlate can be maximum 8 characters")
 	}
 
-	if !regexp.MustCompile("^[A-Za-z]+$").MatchString(newCarReq.Color) {
-		errors = append(errors, "color must be one word only letters")
-	}
-
-	makeAndModelRe := regexp.MustCompile("^[A-Za-z0-9 -]+$")
-	if !makeAndModelRe.MatchString(newCarReq.Make) {
-		errors = append(errors, "make can only have spaces, letters, numbers, and dashes")
-	}
-
-	if !makeAndModelRe.MatchString(newCarReq.Model) {
-		errors = append(errors, "model can only have spaces, letters, numbers, and dashes")
+	if colorMakeModelErrors := invalidColorMakeModel(
+		newCarReq.Color,
+		newCarReq.Make,
+		newCarReq.Model,
+	); len(colorMakeModelErrors) != 0 {
+		errors = append(errors, colorMakeModelErrors...)
 	}
 
 	if len(errors) > 0 {
@@ -87,4 +83,58 @@ func (newCarReq newCarReq) toNewCarArgs() models.NewCarArgs {
 		newCarReq.Make,
 		newCarReq.Model,
 	)
+}
+
+// edit car request
+
+type editCarReq struct {
+	Color string `json:"color"`
+	Make  string `json:"make"`
+	Model string `json:"model"`
+}
+
+func (editCarReq editCarReq) validate() error {
+	if editCarReq.Color == "" && editCarReq.Make == "" && editCarReq.Model == "" {
+		return fmt.Errorf("%w: %v", errInvalidFields, "all edit fields cannot be empty")
+	}
+
+	if colorMakeModelErrors := invalidColorMakeModel(
+		editCarReq.Color,
+		editCarReq.Make,
+		editCarReq.Model,
+	); len(colorMakeModelErrors) != 0 {
+		errors := colorMakeModelErrors
+		return fmt.Errorf("%w: %v", errInvalidFields, strings.Join(errors, ". "))
+	}
+
+	return nil
+}
+
+func (editCarReq editCarReq) toEditCarArgs() models.EditCarArgs {
+	return models.NewEditCarArgs(
+		editCarReq.Color,
+		editCarReq.Make,
+		editCarReq.Model,
+	)
+}
+
+// helpers
+
+func invalidColorMakeModel(color, make, model string) []string {
+	errors := []string{}
+
+	if !regexp.MustCompile("^[A-Za-z]*$").MatchString(color) {
+		errors = append(errors, "color must be one word only letters")
+	}
+
+	makeAndModelRe := regexp.MustCompile("^[A-Za-z0-9 -]*$")
+	if !makeAndModelRe.MatchString(make) {
+		errors = append(errors, "make can only have spaces, letters, numbers, and dashes")
+	}
+
+	if !makeAndModelRe.MatchString(model) {
+		errors = append(errors, "model can only have spaces, letters, numbers, and dashes")
+	}
+
+	return errors
 }
