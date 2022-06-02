95c926ad44a56d42a04eaad029e06ebbd3457e3d
Daniel Velasquez <danivelas4@gmail.com>
2022-06-02 11:36:30 -0400

    api/user.go: added newUser and Equal funcs

--- api/user.go
+++ api/user.go
@@ -17,6 +17,31 @@ type user struct {
 	Role      Role   `json:"role"`
 }
 
+func newUser(id string, firstName string, lastName string, email string, role Role) user {
+	return user{
+		Id:        id,
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Role:      role,
+	}
+}
+
+func (self user) Equal(other user) bool {
+	if self.Id != other.Id {
+		return false
+	} else if self.FirstName != other.FirstName {
+		return false
+	} else if self.LastName != other.LastName {
+		return false
+	} else if self.Email != other.Email {
+		return false
+	} else if self.Role != other.Role {
+		return false
+	}
+	return true
+}
+
 func ctxWithUser(ctx context.Context, user user) context.Context {
 	return context.WithValue(ctx, userCtxKey, user)
 }
