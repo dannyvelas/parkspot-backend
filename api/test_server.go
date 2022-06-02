aa6dc3ad1622b9dc219fb08f6c710c8bb90a305c
Daniel Velasquez <danivelas4@gmail.com>
2022-06-02 11:50:51 -0400

    api/test_server.go: created new helper file/func to initialize test server

---
+++ api/test_server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"fmt"
+	"github.com/dannyvelas/lasvistas_api/config"
+	"github.com/dannyvelas/lasvistas_api/storage"
+	"net/http/httptest"
+)
+
+func newTestServer() (*httptest.Server, error) {
+	config := config.NewConfig()
+
+	database, err := storage.NewDatabase(config.Postgres())
+	if err != nil {
+		return nil, fmt.Errorf("Failed to start database: %v", err)
+	}
+
+	// init repos
+	adminRepo := storage.NewAdminRepo(database)
+	permitRepo := storage.NewPermitRepo(database)
+	carRepo := storage.NewCarRepo(database)
+	residentRepo := storage.NewResidentRepo(database)
+
+	// http setup
+	httpConfig := config.Http()
+
+	router := NewRouter(httpConfig, config.Token(), config.Constants().DateFormat(),
+		adminRepo, permitRepo, carRepo, residentRepo)
+
+	testServer := httptest.NewServer(router)
+
+	return testServer, nil
+}
