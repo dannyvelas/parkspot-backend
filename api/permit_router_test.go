29afd3e9eb715eb9614c150d7881a29f736acb0b
Daniel Velasquez <danivelas4@gmail.com>
2022-06-05 12:47:08 -0400

    api/permit_router_test.go: added testing for getActivePermitsOfResident

--- api/permit_router_test.go
+++ api/permit_router_test.go
@@ -2,20 +2,27 @@ package api
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/dannyvelas/lasvistas_api/config"
+	"github.com/dannyvelas/lasvistas_api/models"
+	"github.com/google/go-cmp/cmp"
 	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/suite"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type permitRouterSuite struct {
 	suite.Suite
-	testServer *httptest.Server
-	jwtToken   string
+	testServer  *httptest.Server
+	jwtToken    string
+	residentId  string
+	existingCar newCarReq
+	newPermit   newPermitReq
 }
 
 func TestPermitRouter(t *testing.T) {
@@ -41,13 +48,22 @@ func (suite *permitRouterSuite) SetupSuite() {
 
 		return jwtToken
 	}()
+
+	suite.residentId = "T1043321"
+	suite.existingCar = newCarReq{"GBTYZME", "green", "ARCTIC CAT", "BEARCAT 2000 LT"}
+	suite.newPermit = newPermitReq{
+		ResidentId:      suite.residentId,
+		Car:             suite.existingCar,
+		StartDate:       time.Now().Truncate(time.Second),
+		EndDate:         time.Now().Add(time.Duration(24) * time.Hour).Truncate(time.Second),
+		ExceptionReason: ""}
 }
 
 func (suite permitRouterSuite) TearDownSuite() {
 	suite.testServer.Close()
 }
 
-func (suite permitRouterSuite) TestCreate_NoStartNoEnd_Negative() {
+func (suite permitRouterSuite) TestCreate_NoStartNoEnd_ErrMissing() {
 	requestBody := []byte(`{
     "residentId":"T1043321",
     "car": {
@@ -85,7 +101,7 @@ func (suite permitRouterSuite) TestCreate_NoStartNoEnd_Negative() {
 	suite.Equal(responseMsg, string(bodyBytes))
 }
 
-func (suite permitRouterSuite) TestCreate_EmptyStartEmptyEnd_Negative() {
+func (suite permitRouterSuite) TestCreate_EmptyStartEmptyEnd_ErrMalformed() {
 	requestBody := []byte(`{
     "residentId":"T1043321",
     "car": {
@@ -123,4 +139,103 @@ func (suite permitRouterSuite) TestCreate_EmptyStartEmptyEnd_Negative() {
 
 	responseMsg := fmt.Sprintf("\"%v\"\n", newErrMalformed("NewPermitReq"))
 	suite.Equal(responseMsg, string(bodyBytes))
+}
+
+func (suite permitRouterSuite) TestGetActivePermitsOfResident_Postive() {
+	permitId, err := createTestPermit(suite.testServer.URL, suite.newPermit, suite.jwtToken)
+	if err != nil {
+		suite.NoError(err)
+		return
+	}
+	defer deleteTestPermit(suite.testServer.URL, permitId, suite.jwtToken)
+
+	endpoint := fmt.Sprintf("%s/api/resident/%s/permits/active", suite.testServer.URL, suite.residentId)
+
+	request, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		suite.NoError(err)
+		return
+	}
+
+	cookie := http.Cookie{Name: "jwt", Value: suite.jwtToken, HttpOnly: true, Path: "/"}
+	request.AddCookie(&cookie)
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		suite.NoError(err)
+		return
+	}
+	defer response.Body.Close()
+
+	suite.Equal(http.StatusOK, response.StatusCode)
+
+	var permitsResponse listWithMetadata[models.Permit]
+	if err := json.NewDecoder(response.Body).Decode(&permitsResponse); err != nil {
+		suite.NoError(err)
+		return
+	} else if len(permitsResponse.Records) == 0 {
+		suite.NotEqual(len(permitsResponse.Records), 0, "no permits found")
+		return
+	}
+
+	last := permitsResponse.Records[len(permitsResponse.Records)-1]
+	suite.Equal(suite.newPermit.ResidentId, last.ResidentId)
+	suite.Equal(suite.newPermit.Car.LicensePlate, last.Car.LicensePlate)
+	suite.Empty(cmp.Diff(last.StartDate, suite.newPermit.StartDate))
+	suite.Empty(cmp.Diff(last.EndDate, suite.newPermit.EndDate))
+}
+
+func createTestPermit(url string, body newPermitReq, jwtToken string) (int, error) {
+	requestBody, err := json.Marshal(body)
+	if err != nil {
+		return 0, nil
+	}
+
+	request, err := http.NewRequest("POST", url+"/api/permit", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return 0, nil
+	}
+
+	cookie := http.Cookie{Name: "jwt", Value: jwtToken, HttpOnly: true, Path: "/"}
+	request.AddCookie(&cookie)
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return 0, nil
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("Status not OK")
+	}
+
+	var newPermitResponse models.Permit
+	if err := json.NewDecoder(response.Body).Decode(&newPermitResponse); err != nil {
+		return 0, nil
+	}
+
+	return newPermitResponse.Id, nil
+}
+
+func deleteTestPermit(url string, id int, jwtToken string) error {
+	endpoint := fmt.Sprintf("%s/api/permit/%d", url, id)
+	request, err := http.NewRequest("DELETE", endpoint, nil)
+	if err != nil {
+		return err
+	}
+
+	cookie := http.Cookie{Name: "jwt", Value: jwtToken, HttpOnly: true, Path: "/"}
+	request.AddCookie(&cookie)
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("Status not OK")
+	}
+
+	return nil
 }
