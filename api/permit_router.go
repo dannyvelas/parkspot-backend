3904752fc403ed3dd478e508660dc155f8e2f589
Daniel Velasquez <danivelas4@gmail.com>
2022-06-05 12:45:49 -0400

    api/permit_router.go: added getActivePermitsOfResident func

--- api/permit_router.go
+++ api/permit_router.go
@@ -54,6 +54,27 @@ func getOne(permitRepo storage.PermitRepo) http.HandlerFunc {
 	}
 }
 
+func getActivePermitsOfResident(permitRepo storage.PermitRepo) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		if id == "" {
+			respondError(w, newErrBadRequest("id parameter cannot be empty"))
+			return
+		}
+
+		permits, err := permitRepo.GetActiveOfResident(id)
+		if err != nil {
+			log.Error().Msgf("permit_router.getActivePermitsOfResident: Error getting permits: %v", err)
+			respondInternalError(w)
+			return
+		}
+
+		permitsWithMetadata := newListWithMetadata(permits, len(permits))
+
+		respondJSON(w, http.StatusOK, permitsWithMetadata)
+	}
+}
+
 func create(permitRepo storage.PermitRepo, carRepo storage.CarRepo, residentRepo storage.ResidentRepo, dateFormat string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var newPermitReq newPermitReq
