a2e7a36f48f4b09fa0a1171f81eddb04299919dc
Daniel Velasquez <danivelas4@gmail.com>
2022-07-09 22:32:53 -0400

    scripts/find_permitresids_notin_residentresids.py: added some logging

--- scripts/find_permitresids_notin_residentresids.py
+++ scripts/find_permitresids_notin_residentresids.py
@@ -2,21 +2,37 @@ import re
 
 permit_resids = set()
 resident_resids = set()
-with open('./migrations/000005_seed_permit.up.sql', 'r') as pfile_in:
-    for i, line in enumerate(pfile_in):
-        match = re.findall(r'(t|T|b|B)([0-9]+)', line)
+with open('./.prodmigrations/000005_seed_permit.up.sql', 'r') as pfile_in:
+    amt_lines_read = 0
+    amt_matches = 0
+    for line in pfile_in:
+        match = re.findall(r'(T|B)([0-9]+)', line)
         if match:
             first_match = match[0]
             res_id = first_match[0] + first_match[1]
             permit_resids.add(res_id)
-    with open('./migrations/000004_seed_resident.up.sql', 'r') as rfile_in:
-        for line in rfile_in:
-            match = re.findall(r'(t|T|b|B)([0-9]+)', line)
-            if match:
-                first_match = match[0]
-                res_id = first_match[0] + first_match[1]
-                resident_resids.add(res_id)
+            amt_matches += 1
+        amt_lines_read += 1
+    print(f'amt_lines_read: {amt_lines_read}. amt_matches: {amt_matches}')
 
+with open('./.prodmigrations/000004_seed_resident.up.sql', 'r') as rfile_in:
+    amt_lines_read = 0
+    amt_matches = 0
+    for line in rfile_in:
+        match = re.findall(r'(T|B)([0-9]+)', line)
+        if match:
+            first_match = match[0]
+            res_id = first_match[0] + first_match[1]
+            resident_resids.add(res_id)
+            amt_matches += 1
+        amt_lines_read += 1
+    print(f'amt_lines_read: {amt_lines_read}. amt_matches: {amt_matches}')
+
+amt_here = 0
 for res_id in permit_resids:
     if res_id not in resident_resids:
         print(res_id)
+    else:
+        amt_here = amt_here + 1
+
+print(f'amt_here: {amt_here}')
