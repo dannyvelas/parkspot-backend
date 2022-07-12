fc5d29b604704d91ae991c47688f5867376f3149
Daniel Velasquez <danivelas4@gmail.com>
2022-07-11 22:28:01 -0400

    storage/admin_repo.go: GetOne selecting by id and not email

--- storage/admin_repo.go
+++ storage/admin_repo.go
@@ -19,7 +19,7 @@ func (adminRepo AdminRepo) GetOne(id string) (models.Admin, error) {
 		return models.Admin{}, fmt.Errorf("admin_repo.GetOne: %w: Empty ID argument", ErrInvalidArg)
 	}
 
-	const query = `SELECT id, first_name, last_name, email, password, is_privileged FROM admin WHERE email = $1`
+	const query = `SELECT id, first_name, last_name, email, password, is_privileged FROM admin WHERE id = $1`
 
 	var admin admin
 	err := adminRepo.database.driver.Get(&admin, query, id)
