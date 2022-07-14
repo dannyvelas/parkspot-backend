ac5d8522103540cfbafa3cae17a9f0ad4d94865e
Daniel Velasquez <danivelas4@gmail.com>
2022-07-14 13:04:29 -0400

    scripts/verify/find_permitlps_notin_carlps.py: added some logging

--- scripts/verify/find_permitlps_notin_carlps.py
+++ scripts/verify/find_permitlps_notin_carlps.py
@@ -2,22 +2,32 @@ import re
 
 permit_lps = set()
 car_lps = set()
-with open('./migrations/000005_seed_permit.up.sql', 'r') as pfile_in:
+with open('./.prodmigrations/000005_seed_permit.up.sql', 'r') as pfile_in:
+    amt_matches = 0
     for line in pfile_in:
         match = re.findall(r"= '([^']+)'", line)
         if match:
             first_match = match[0]
             permit_lps.add(first_match)
-    with open('./migrations/000003_seed_car.up.sql', 'r') as cfile_in:
-        for line in cfile_in:
-            match = re.findall(r", '([^']+)'", line)
-            if match:
-                first_match = match[0]
-                car_lps.add(first_match)
+            amt_matches += 1
+    print(amt_matches)
+with open('./.prodmigrations/000003_seed_car.up.sql', 'r') as cfile_in:
+    amt_matches = 0
+    for line in cfile_in:
+        match = re.findall(r", '([^']+)'", line)
+        if match:
+            first_match = match[0]
+            car_lps.add(first_match)
+            amt_matches += 1
+    print(amt_matches)
 
 amt_not_in_car_lps = 0
+amt_here = 0
 for lp in permit_lps:
     if lp not in car_lps:
         amt_not_in_car_lps += 1
         print(lp)
+    else:
+        amt_here += 1
 print('amt not of lps not in car.up.sql:', amt_not_in_car_lps)
+print(f'amt_here: {amt_here}')
