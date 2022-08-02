a39e4a460c203014a14869eeb4159ce7269c804e
Daniel Velasquez <danivelas4@gmail.com>
2022-08-02 19:16:44 -0400

    api/jwt_middleware.go: making tokens expire on a weekly basis

--- api/jwt_middleware.go
+++ api/jwt_middleware.go
@@ -30,7 +30,7 @@ func NewJWTMiddleware(tokenConfig config.TokenConfig) jwtMiddleware {
 func (jwtMiddleware jwtMiddleware) newJWT(id string, firstName string, lastName string, email string, role role) (string, error) {
 	claims := jwtClaims{
 		user{id, firstName, lastName, email, role},
-		jwt.StandardClaims{ExpiresAt: time.Now().Add(time.Hour * 12).Unix()},
+		jwt.StandardClaims{ExpiresAt: time.Now().Add(time.Hour * 24 * 7).Unix()},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
