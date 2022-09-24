1b5da69e6d4c10aa40b5eaf137587f8fe6ec7930
Daniel Velasquez <danivelas4@gmail.com>
2022-09-24 13:39:39 -0400

    config/postgres.go: specifying sslmode disable

--- config/postgres.go
+++ config/postgres.go
@@ -6,7 +6,7 @@ type PostgresConfig struct {
 
 func newPostgresConfig() PostgresConfig {
 	return PostgresConfig{
-		url: readEnvString("DATABASE_URL", "postgres://postgres:postgres@127.0.0.1:5432/postgres"),
+		url: readEnvString("DATABASE_URL", "postgresql://postgres:postgres@127.0.0.1:5432/postgres?sslmode=disable"),
 	}
 }
 
