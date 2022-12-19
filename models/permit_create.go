fe0565c549ff7a4127b2d0e2068520acd459360f
Daniel Velasquez <danivelas4@gmail.com>
2022-12-19 16:41:32 -0500

    models/permit_create.go: removed unnecessary fields

--- models/permit_create.go
+++ models/permit_create.go
@@ -2,9 +2,7 @@ package models
 
 type CreatePermit struct {
 	ResidentId      string
-	CarId           string
 	StartDate       int64
 	EndDate         int64
-	AffectsDays     bool
 	ExceptionReason string
 }
