dc6a76588a2f9f0b07a9fb96a990b6b6c08687e3
Daniel Velasquez <danivelas4@gmail.com>
2022-12-19 16:41:15 -0500

    models/car_create.go: removed unnecessary JSON annotation

--- models/car_create.go
+++ models/car_create.go
@@ -1,8 +1,8 @@
 package models
 
 type CreateCar struct {
-	LicensePlate string `json:"licensePlate"`
-	Color        string `json:"color"`
-	Make         string `json:"make"`
-	Model        string `json:"model"`
+	LicensePlate string
+	Color        string
+	Make         string
+	Model        string
 }
