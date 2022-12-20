c565bce9a352fa55eccd4af968dfed7068b7d232
Daniel Velasquez <danivelas4@gmail.com>
2022-12-20 17:35:48 -0500

    scripts/gen/test_data.py: made permit field names shorter

--- scripts/gen/test_data.py
+++ scripts/gen/test_data.py
@@ -56,23 +56,23 @@ def get_rand_line(file_name: str) -> str:
 class Permit:
     id: int
     resident_id: str
-    car_license_plate: str
-    car_color: str
-    car_make: Union[str, None]
-    car_model: Union[str, None]
+    license_plate: str
+    color: str
+    make: Union[str, None]
+    model: Union[str, None]
     start_ts: int
     end_ts: int
     request_ts: Union[int, None]
     affects_days: bool
     exception_reason: Union[str, None]
 
-    def __init__(self, id: int, resident_id: str, car_license_plate: str, car_color: str, car_make: Union[str, None], car_model: Union[str, None], start_ts: int, end_ts: int, request_ts: Union[int, None], affects_days: bool, exception_reason: Union[str, None]):
+    def __init__(self, id: int, resident_id: str, license_plate: str, color: str, make: Union[str, None], model: Union[str, None], start_ts: int, end_ts: int, request_ts: Union[int, None], affects_days: bool, exception_reason: Union[str, None]):
         self.id = id
         self.resident_id = resident_id
-        self.car_license_plate = car_license_plate
-        self.car_color = car_color
-        self.car_make = car_make
-        self.car_model = car_model
+        self.license_plate = license_plate
+        self.color = color
+        self.make = make
+        self.model = model
         self.start_ts = start_ts
         self.end_ts = end_ts
         self.request_ts = request_ts
@@ -82,13 +82,13 @@ class Permit:
     def as_sql(self) -> str:
         escaped_reason = self.exception_reason.replace(
             "'", "''") if self.exception_reason else None
-        return (f"INSERT INTO permit(id, resident_id, car_license_plate, car_color, car_make, car_model, start_ts, end_ts, request_ts, affects_days, exception_reason) VALUES"
+        return (f"INSERT INTO permit(id, resident_id, license_plate, color, make, model, start_ts, end_ts, request_ts, affects_days, exception_reason) VALUES"
                 f"( {self.id}"
                 f", '{self.resident_id}'"
-                f", '{self.car_license_plate}'"
-                f", '{self.car_color}'"
-                f", {nullable_to_sql(self.car_make)}"
-                f", {nullable_to_sql(self.car_model)}"
+                f", '{self.license_plate}'"
+                f", '{self.color}'"
+                f", {nullable_to_sql(self.make)}"
+                f", {nullable_to_sql(self.model)}"
                 f", {self.start_ts}"
                 f", {self.end_ts}"
                 f", {nullable_to_sql(self.request_ts)}"
@@ -99,10 +99,10 @@ class Permit:
         return (
             f"{self.id}"
             f"\t{self.resident_id}"
-            f"\t{self.car_license_plate}"
-            f"\t{self.car_color}"
-            f"\t{nullable_to_csv(self.car_make)}"
-            f"\t{nullable_to_csv(self.car_model)}"
+            f"\t{self.license_plate}"
+            f"\t{self.color}"
+            f"\t{nullable_to_csv(self.make)}"
+            f"\t{nullable_to_csv(self.model)}"
             f"\t{self.start_ts}"
             f"\t{self.end_ts}"
             f"\t{nullable_to_csv(self.request_ts)}"
@@ -115,10 +115,10 @@ def row_to_permit(row: List[str]) -> Permit:
     return Permit(
         id=int(row[0]),
         resident_id=row[1],
-        car_license_plate=row[2],
-        car_color=row[3],
-        car_make=row[4],
-        car_model=row[5],
+        license_plate=row[2],
+        color=row[3],
+        make=row[4],
+        model=row[5],
         start_ts=int(row[6]),
         end_ts=int(row[7]),
         request_ts=int(row[8]) if row[8] != '' else None,
@@ -127,7 +127,7 @@ def row_to_permit(row: List[str]) -> Permit:
     )
 
 
-def get_rand_permit(i: int, resident_id: str, car_license_plate: str, car_color: str, car_make: Union[str, None], car_model: Union[str, None]) -> Permit:
+def get_rand_permit(i: int, resident_id: str, license_plate: str, color: str, make: Union[str, None], model: Union[str, None]) -> Permit:
     def get_rand_sentance() -> str:
         with open('./scripts/gen/csv_in/sentances.csv', 'r') as in_file:
             random_line = next(in_file)
@@ -142,10 +142,10 @@ def get_rand_permit(i: int, resident_id: str, car_license_plate: str, car_color:
     return Permit(
         i,
         resident_id,
-        car_license_plate,
-        car_color,
-        car_make,
-        car_model,
+        license_plate,
+        color,
+        make,
+        model,
         start_ts,
         end_ts,
         int(request_ts) if bool(random.getrandbits(1)) else None,
