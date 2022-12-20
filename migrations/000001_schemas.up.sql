30aac4c6b9eb06123b2875eba95ab281e0a05177
Daniel Velasquez <danivelas4@gmail.com>
2022-12-20 17:34:00 -0500

    migrations/000001_schemas.up.sql: shortened permit field names

--- migrations/000001_schemas.up.sql
+++ migrations/000001_schemas.up.sql
@@ -39,10 +39,10 @@ CREATE TABLE IF NOT EXISTS car(
 -- thus, we want the car information in each permit to be a "snapshot", at the time the permit was created
 CREATE TABLE IF NOT EXISTS permit(
   id SERIAL PRIMARY KEY UNIQUE NOT NULL,
-  car_license_plate VARCHAR(10) NOT NULL,
-  car_color TEXT NOT NULL,
-  car_make TEXT,
-  car_model TEXT,
+  license_plate VARCHAR(10) NOT NULL,
+  color TEXT NOT NULL,
+  make TEXT,
+  model TEXT,
   resident_id CHAR(8) REFERENCES resident(id) ON DELETE CASCADE NOT NULL,
   start_ts BIGINT NOT NULL,
   end_ts BIGINT NOT NULL,
