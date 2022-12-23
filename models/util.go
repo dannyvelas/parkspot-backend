2434d43e5b4dfcd99baa19c1002fa0c2d35c80ab
Daniel Velasquez <danivelas4@gmail.com>
2022-12-23 18:14:00 -0500

    models/util.go: added

---
+++ models/util.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"regexp"
+)
+
+func getColorMakeModelErrors(color, make, model string) []string {
+	errors := []string{}
+
+	if !regexp.MustCompile("^[A-Za-z]*$").MatchString(color) {
+		errors = append(errors, "color must be one word only letters")
+	}
+
+	makeAndModelRe := regexp.MustCompile("^[A-Za-z0-9 -]*$")
+	if !makeAndModelRe.MatchString(make) {
+		errors = append(errors, "make can only have spaces, letters, numbers, and dashes")
+	}
+
+	if !makeAndModelRe.MatchString(model) {
+		errors = append(errors, "model can only have spaces, letters, numbers, and dashes")
+	}
+
+	return errors
+}
