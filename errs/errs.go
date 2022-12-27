e6c025ef7483259e8afb32f7378750375c9a6058
Daniel Velasquez <danivelas4@gmail.com>
2022-12-26 21:53:41 -0500

    merged api/errors into errs/errs.go

--- errs/errs.go
+++ errs/errs.go
@@ -5,10 +5,12 @@ import (
 	"github.com/dannyvelas/lasvistas_api/config"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
+	"net/http"
 )
 
 type ApiErr struct {
-	message string
+	statusCode int
+	message    string
 }
 
 // implements error interface
@@ -17,18 +19,25 @@ func (e ApiErr) Error() string {
 }
 
 var (
-	EmptyFields          = ApiErr{"One or more missing fields"}
-	InvalidFields        = ApiErr{"One or more invalid fields"}
-	Unauthorized         = ApiErr{"unauthorized"}
-	NotFound             = ApiErr{"not found"}
-	AlreadyExists        = ApiErr{"already exists"}
-	ResidentForPermitDNE = ApiErr{"Users must have a registered account to request" +
-		" a guest parking permit. Please create their account before requesting their permit."}
-	CarForPermitDNE = ApiErr{"The car that you chose for this permit does not" +
-		" exist. Please create or choose another car."}
-	CarActivePermit = ApiErr{"Cannot create a permit during these dates" +
-		" because this car has at least one active permit during that time."}
+	EmptyFields          = ApiErr{http.StatusBadRequest, "One or more missing fields"}
+	InvalidFields        = ApiErr{http.StatusBadRequest, "One or more invalid fields"}
+	Unauthorized         = ApiErr{http.StatusUnauthorized, "unauthorized"}
+	NotFound             = ApiErr{http.StatusNotFound, "not found"}
+	AlreadyExists        = ApiErr{http.StatusBadRequest, "already exists"}
+	ResidentForPermitDNE = ApiErr{
+		statusCode: http.StatusBadRequest,
+		message: "Users must have a registered account to request a guest parking" +
+			" permit. Please create their account before requesting their permit."}
+	CarForPermitDNE = ApiErr{
+		statusCode: http.StatusBadRequest,
+		message: "The car that you chose for this permit does not" +
+			" exist. Please create or choose another car."}
+	CarActivePermit = ApiErr{
+		statusCode: http.StatusBadRequest,
+		message: "Cannot create a permit during these dates" +
+			" because this car has at least one active permit during that time."}
 	PermitTooLong = ApiErr{
+		statusCode: http.StatusBadRequest,
 		message: fmt.Sprintf("Error: Requests cannot be longer than %d days,"+
 			" unless there is an exception."+
 			"\nIf this resident wants their guest to park for more than %d days, they"+
@@ -36,8 +45,13 @@ var (
 			config.MaxPermitLength,
 			config.MaxPermitLength),
 	}
-	ResidentTwoActivePermits = ApiErr{"Cannot create a permit during these dates" +
-		" because this resident has at least two active permits during that time."}
+	ResidentTwoActivePermits = ApiErr{
+		statusCode: http.StatusBadRequest,
+		message: "Cannot create a permit during these dates" +
+			" because this resident has at least two active permits during that time."}
+	InternalServerError = ApiErr{
+		statusCode: http.StatusInternalServerError,
+		message:    "Internal Server Error"}
 )
 
 func NewAlreadyExists(resource string) error {
@@ -49,6 +63,7 @@ func EntityDaysTooLong(entity string, amtDaysUsed int) ApiErr {
 	entityTitle := cases.Title(language.English).String(entity)
 
 	return ApiErr{
+		statusCode: http.StatusBadRequest,
 		message: fmt.Sprintf("Error: This %s has used parking permits that have lasted"+
 			" a combined total of %d days."+
 			"\n%ss are allowed maximum %d days of parking passes, unless there is an exception."+
@@ -63,6 +78,7 @@ func PermitPlusEntityDaysTooLong(entity string, amtDaysUsed int) ApiErr {
 	entityLower := cases.Lower(language.English).String(entity)
 
 	return ApiErr{
+		statusCode: http.StatusBadRequest,
 		message: fmt.Sprintf("Error: This request would exceed the %s's"+
 			" yearly guest parking pass limit of %d days."+
 			"\nThis %s has given out parking permits for a total of %d days."+
@@ -73,4 +89,8 @@ func PermitPlusEntityDaysTooLong(entity string, amtDaysUsed int) ApiErr {
 			entityLower, amtDaysUsed,
 			entityLower, config.MaxParkingDays-amtDaysUsed,
 			entityLower)}
+}
+
+func Malformed(payload string) ApiErr {
+	return ApiErr{http.StatusBadRequest, payload + " malformed"}
 }
