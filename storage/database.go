4eae7f6e2fa6208ffaf7b49556aebd3a01861292
Daniel Velasquez <danivelas4@gmail.com>
2022-12-28 10:59:22 -0500

    storage/database.go: using errors from errs/database.go instead of storage/errors.go

--- storage/database.go
+++ storage/database.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"fmt"
 	"github.com/dannyvelas/lasvistas_api/config"
+	"github.com/dannyvelas/lasvistas_api/errs"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -14,7 +15,7 @@ type Database struct {
 func NewDatabase(postgresConfig config.PostgresConfig) (Database, error) {
 	driver, err := sqlx.Connect("postgres", postgresConfig.URL())
 	if err != nil {
-		return Database{}, fmt.Errorf("database: %w: %v", ErrConnecting, err)
+		return Database{}, fmt.Errorf("database: %w: %v", errs.DBConnecting, err)
 	}
 
 	return Database{driver: driver}, nil
