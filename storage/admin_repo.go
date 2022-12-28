0714a830244e7b7960a43aa82af650f5c9b7ec61
Daniel Velasquez <danivelas4@gmail.com>
2022-12-28 10:55:27 -0500

    storage/admin_repo.go: using errors from errs/database.go instead of storage/errors.go

--- storage/admin_repo.go
+++ storage/admin_repo.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"database/sql"
 	"fmt"
+	"github.com/dannyvelas/lasvistas_api/errs"
 	"github.com/dannyvelas/lasvistas_api/models"
 )
 
@@ -16,7 +17,7 @@ func NewAdminRepo(database Database) AdminRepo {
 
 func (adminRepo AdminRepo) GetOne(id string) (models.Admin, error) {
 	if id == "" {
-		return models.Admin{}, fmt.Errorf("admin_repo.GetOne: %w: Empty ID argument", ErrInvalidArg)
+		return models.Admin{}, fmt.Errorf("admin_repo.GetOne: %w: Empty ID", errs.DBInvalidArg)
 	}
 
 	const query = `
@@ -29,9 +30,9 @@ func (adminRepo AdminRepo) GetOne(id string) (models.Admin, error) {
 	var admin admin
 	err := adminRepo.database.driver.Get(&admin, query, id)
 	if err == sql.ErrNoRows {
-		return models.Admin{}, fmt.Errorf("admin_repo.GetOne: %w", ErrNoRows)
+		return models.Admin{}, fmt.Errorf("admin_repo.GetOne: %w", errs.NotFound)
 	} else if err != nil {
-		return models.Admin{}, fmt.Errorf("admin_repo.GetOne: %w: %v", ErrQueryScanOneRow, err)
+		return models.Admin{}, fmt.Errorf("admin_repo.GetOne: %w: %v", errs.DBQueryScanOneRow, err)
 	}
 
 	return admin.toModels(), nil
@@ -39,15 +40,15 @@ func (adminRepo AdminRepo) GetOne(id string) (models.Admin, error) {
 
 func (adminRepo AdminRepo) SetPassword(id string, password string) error {
 	if id == "" {
-		return fmt.Errorf("admin_repo.GetOne: %w: Empty ID argument", ErrInvalidArg)
+		return fmt.Errorf("admin_repo.GetOne: %w: Empty ID argument", errs.DBInvalidArg)
 	} else if password == "" {
-		return fmt.Errorf("admin_repo.GetOne: %w: Emtpy Password argument", ErrInvalidArg)
+		return fmt.Errorf("admin_repo.GetOne: %w: Emtpy Password argument", errs.DBInvalidArg)
 	}
 
 	const query = `UPDATE admin SET password = $1 WHERE id = $2`
 	_, err := adminRepo.database.driver.Exec(query, password, id)
 	if err != nil {
-		return fmt.Errorf("admin_repo.SetPasswordFor: %w: %v", ErrDatabaseExec, err)
+		return fmt.Errorf("admin_repo.SetPasswordFor: %w: %v", errs.DBExec, err)
 	}
 
 	return nil
