f4bbf32df41d3140a33ea106203d0a7b8f576bb9
Daniel Velasquez <danivelas4@gmail.com>
2022-12-28 23:17:13 -0500

    errs/permit_create.go: using NewApiErr instead of struct literal

--- errs/permit_create.go
+++ errs/permit_create.go
@@ -9,55 +9,55 @@ import (
 )
 
 var (
-	ResidentForPermitDNE = &ApiErr{
-		StatusCode: http.StatusBadRequest,
-		message: "Users must have a registered account to request a guest parking" +
-			" permit. Please create their account before requesting their permit."}
-	CarForPermitDNE = &ApiErr{
-		StatusCode: http.StatusBadRequest,
-		message: "The car that you chose for this permit does not" +
-			" exist. Please create or choose another car."}
-	CarActivePermit = &ApiErr{
-		StatusCode: http.StatusBadRequest,
-		message: "Cannot create a permit during these dates" +
-			" because this car has at least one active permit during that time."}
-	PermitTooLong = &ApiErr{
-		StatusCode: http.StatusBadRequest,
-		message: fmt.Sprintf("Error: Requests cannot be longer than %d days,"+
+	ResidentForPermitDNE = NewApiErr(
+		http.StatusBadRequest,
+		"Users must have a registered account to request a guest parking"+
+			" permit. Please create their account before requesting their permit.")
+	CarForPermitDNE = NewApiErr(
+		http.StatusBadRequest,
+		"The car that you chose for this permit does not"+
+			" exist. Please create or choose another car.")
+	CarActivePermit = NewApiErr(
+		http.StatusBadRequest,
+		"Cannot create a permit during these dates"+
+			" because this car has at least one active permit during that time.")
+	PermitTooLong = NewApiErr(
+		http.StatusBadRequest,
+		fmt.Sprintf("Error: Requests cannot be longer than %d days,"+
 			" unless there is an exception."+
 			"\nIf this resident wants their guest to park for more than %d days, they"+
 			" can apply for another request once that one expires.",
 			config.MaxPermitLength,
 			config.MaxPermitLength),
-	}
-	ResidentTwoActivePermits = &ApiErr{
-		StatusCode: http.StatusBadRequest,
-		message: "Cannot create a permit during these dates" +
-			" because this resident has at least two active permits during that time."}
+	)
+	ResidentTwoActivePermits = NewApiErr(
+		http.StatusBadRequest,
+		"Cannot create a permit during these dates"+
+			" because this resident has at least two active permits during that time.")
 )
 
 func EntityDaysTooLong(entity string, amtDaysUsed int) *ApiErr {
 	entityLower := cases.Lower(language.English).String(entity)
 	entityTitle := cases.Title(language.English).String(entity)
 
-	return &ApiErr{
-		StatusCode: http.StatusBadRequest,
-		message: fmt.Sprintf("Error: This %s has used parking permits that have lasted"+
+	return NewApiErr(
+		http.StatusBadRequest,
+		fmt.Sprintf("Error: This %s has used parking permits that have lasted"+
 			" a combined total of %d days."+
 			"\n%ss are allowed maximum %d days of parking passes, unless there is an exception."+
 			"\nThis %s must wait until next year to give out new parking passes.",
 			entityLower, amtDaysUsed,
 			entityTitle, config.MaxParkingDays,
 			entityLower),
-	}
+	)
 }
 
 func PermitPlusEntityDaysTooLong(entity string, amtDaysUsed int) *ApiErr {
 	entityLower := cases.Lower(language.English).String(entity)
 
-	return &ApiErr{
-		StatusCode: http.StatusBadRequest,
-		message: fmt.Sprintf("Error: This request would exceed the %s's"+
+	return NewApiErr(
+		http.StatusBadRequest,
+		fmt.Sprintf("Error: This request would exceed the %s's"+
 			" yearly guest parking pass limit of %d days."+
 			"\nThis %s has given out parking permits for a total of %d days."+
 			"\nThis %s can give out max %d more day(s) before reaching their limit."+
@@ -66,5 +66,5 @@ func PermitPlusEntityDaysTooLong(entity string, amtDaysUsed int) *ApiErr {
 			entityLower, config.MaxParkingDays,
 			entityLower, amtDaysUsed,
 			entityLower, config.MaxParkingDays-amtDaysUsed,
-			entityLower)}
+			entityLower))
 }
