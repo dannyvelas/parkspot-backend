7483541e325bfaff322519f33fe1f416eabea794
Daniel Velasquez <danivelas4@gmail.com>
2022-12-28 23:15:52 -0500

    errs/request.go: started using NewApiErr. also making NewNotFound func wrap NotFound sentinel, while still keeping its return type as *ApiErr. this allows errors.As(err, &apiErr); apiErr.Error() to return "<resource> not found" instead of just "not found"

--- errs/request.go
+++ errs/request.go
@@ -6,30 +6,30 @@ import (
 )
 
 var (
-	Unauthorized = &ApiErr{http.StatusUnauthorized, "unauthorized"}
-	NotFound     = &ApiErr{http.StatusNotFound, "not found"}
+	Unauthorized = NewApiErr(http.StatusUnauthorized, "unauthorized")
+	NotFound     = NewApiErr(http.StatusNotFound, "not found")
 )
 
 func BadRequest(message string) *ApiErr {
-	return &ApiErr{http.StatusBadRequest, message}
+	return NewApiErr(http.StatusBadRequest, message)
 }
 
-func NewNotFound(resource string) error {
-	return fmt.Errorf("%s %w", resource, NotFound)
+func NewNotFound(resource string) *ApiErr {
+	return &ApiErr{http.StatusNotFound, fmt.Errorf("%s %w", resource, NotFound)}
 }
 
 func EmptyFields(fields string) *ApiErr {
-	return &ApiErr{http.StatusBadRequest, "One or more missing fields: " + fields}
+	return NewApiErr(http.StatusBadRequest, "One or more missing fields: "+fields)
 }
 
 func InvalidFields(fields string) *ApiErr {
-	return &ApiErr{http.StatusBadRequest, "One or more invalid fields: " + fields}
+	return NewApiErr(http.StatusBadRequest, "One or more invalid fields: "+fields)
 }
 
 func Malformed(payload string) *ApiErr {
-	return &ApiErr{http.StatusBadRequest, payload + " malformed"}
+	return NewApiErr(http.StatusBadRequest, payload+" malformed")
 }
 
 func AlreadyExists(resource string) *ApiErr {
-	return &ApiErr{http.StatusBadRequest, resource + " already exists"}
+	return NewApiErr(http.StatusBadRequest, resource+" already exists")
 }
