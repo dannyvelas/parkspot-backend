460dfc235395378c35292414369ef97e1ae89342
Daniel Velasquez <danivelas4@gmail.com>
2023-04-07 14:39:46 -0400

    storage/selector.go: separated limit/offset setting in selector

--- storage/selector.go
+++ storage/selector.go
@@ -64,10 +64,11 @@ func WithLimitAndOffset(limit, offset int) limitAndOffset {
 	return limitAndOffset{limit, offset}
 }
 func (limitAndOffset limitAndOffset) dispatch(repo selectableRepo, selector squirrel.SelectBuilder) squirrel.SelectBuilder {
-	if limitAndOffset.limit >= 0 && limitAndOffset.offset >= 0 {
-		return selector.
-			Limit(uint64(getBoundedLimit(limitAndOffset.limit))).
-			Offset(uint64(limitAndOffset.offset))
+	if limitAndOffset.limit >= 0 {
+		selector = selector.Limit(uint64(getBoundedLimit(limitAndOffset.limit)))
+	}
+	if limitAndOffset.offset >= 0 {
+		selector = selector.Offset(uint64(limitAndOffset.offset))
 	}
 	return selector
 }
