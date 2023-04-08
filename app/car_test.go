d71a172828927361c86010a60768db0c889d7135
Daniel Velasquez <danivelas4@gmail.com>
2023-04-08 15:23:55 -0400

    app/car_test.go: created tests from api/car_handler_test.go, which i now deleted. to get this to work, created storage/car_repo_mock.go

---
+++ app/car_test.go
@@ -0,0 +1,102 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"github.com/dannyvelas/lasvistas_api/errs"
+	"github.com/dannyvelas/lasvistas_api/models"
+	"github.com/dannyvelas/lasvistas_api/storage"
+	"github.com/imdario/mergo"
+	"github.com/stretchr/testify/suite"
+	"net/http"
+	"testing"
+)
+
+type carTestSuite struct {
+	suite.Suite
+	carService CarService
+}
+
+func TestCarService(t *testing.T) {
+	suite.Run(t, new(carTestSuite))
+}
+
+func (suite *carTestSuite) SetupSuite() {
+	carRepo := storage.NewCarRepoMock()
+	suite.carService = NewCarService(&carRepo)
+}
+
+func (suite carTestSuite) TestEdit_CarDNE_Negative() {
+	carWithIDThatDNE := models.Car{ID: "9b6d89a6-0b66-4170-be8d-eba43f8bf478", LicensePlate: "NEWLP"}
+
+	_, err := suite.carService.Update(carWithIDThatDNE)
+	if err == nil {
+		suite.NoError(fmt.Errorf("No error encountered when editing a non-existing car"))
+		return
+	}
+
+	var apiErr *errs.ApiErr
+	if !errors.As(err, &apiErr) {
+		suite.NoError(fmt.Errorf("Couldn't cast error to apiErr. Error is: %v", err))
+		return
+	}
+
+	suite.Equal(http.StatusNotFound, apiErr.StatusCode, "response was: %v", apiErr.Error())
+}
+
+func (suite carTestSuite) TestEdit_Car_Positive() {
+	carToEdit := models.NewCar("d1e0affb-14e7-4e9f-b8a3-70be7d49d063", "B0000000", "lp1", "color", "make", "model", 0)
+
+	// set up a table of tests
+	type test struct {
+		argument models.Car
+		expected models.Car
+	}
+	tests := map[string]test{
+		"licensePlate":              {argument: models.Car{ID: carToEdit.ID, LicensePlate: "NEWLP"}},
+		"licensePlate, color":       {argument: models.Car{ID: carToEdit.ID, LicensePlate: "NEWLP", Color: "NEWCOLOR"}},
+		"licensePlate, color, make": {argument: models.Car{ID: carToEdit.ID, LicensePlate: "NEWLP", Color: "NEWCOLOR", Make: "NEWMAKE"}},
+		"model":                     {argument: models.Car{ID: carToEdit.ID, Model: "NEWMODEL"}},
+	}
+	for testName, test_ := range tests {
+		expected := test_.argument
+		if err := mergo.Merge(&expected, carToEdit); err != nil {
+			suite.NoError(fmt.Errorf("error merging when preparing test: %v\n", err))
+			return
+		}
+		tests[testName] = test{argument: test_.argument, expected: expected}
+	}
+
+	// this func will execute one row of above table
+	executeTest := func(test test) error {
+		result, err := suite.carService.Update(test.argument)
+		if err != nil {
+			return fmt.Errorf("Error making request: %v", err)
+		}
+
+		suite.Equal(test.expected.ID, result.ID)
+		suite.Equal(test.expected.LicensePlate, result.LicensePlate)
+		suite.Equal(test.expected.Color, result.Color)
+		suite.Equal(test.expected.Make, result.Make)
+		suite.Equal(test.expected.Model, result.Model)
+
+		return nil
+	}
+
+	for testName, test := range tests {
+		createdCar, err := suite.carService.Create(carToEdit)
+		if err != nil {
+			suite.NoError(fmt.Errorf("Error creating test car before running test: %v", err))
+			break
+		}
+
+		if err := executeTest(test); err != nil {
+			suite.NoError(fmt.Errorf("%s failed: %v", testName, err))
+		}
+
+		if err := suite.carService.Delete(createdCar.ID); err != nil {
+			suite.NoError(fmt.Errorf("Error deleting test car after running test: %v", err))
+			break
+		}
+	}
+}
---
+++ storage/car_repo_mock.go
@@ -0,0 +1,103 @@
+package storage
+
+import (
+	"github.com/dannyvelas/lasvistas_api/errs"
+	"github.com/dannyvelas/lasvistas_api/models"
+	"github.com/dannyvelas/lasvistas_api/util"
+	"github.com/google/uuid"
+)
+
+type CarRepoMock struct {
+	cars []models.Car
+}
+
+func NewCarRepoMock() CarRepoMock {
+	return CarRepoMock{cars: make([]models.Car, 0)}
+}
+
+func (carRepoMock *CarRepoMock) GetOne(id string) (models.Car, error) {
+	i := util.Find(carRepoMock.cars, func(car models.Car) bool { return car.ID == id })
+	if i < 0 {
+		return models.Car{}, errs.NewNotFound("car")
+	}
+	return carRepoMock.cars[i], nil
+}
+
+func (carRepoMock *CarRepoMock) SelectWhere(carFields models.Car) ([]models.Car, error) {
+	var carsFound []models.Car
+	for _, car := range carRepoMock.cars {
+		if (carFields.ID == "" || carFields.ID == car.ID) &&
+			(carFields.ResidentID == "" || carFields.ResidentID == car.ResidentID) &&
+			(carFields.LicensePlate == "" || carFields.LicensePlate == car.LicensePlate) &&
+			(carFields.Color == "" || carFields.Color == car.Color) &&
+			(carFields.Make == "" || carFields.Make == car.Make) &&
+			(carFields.Model == "" || carFields.Model == car.Model) {
+			carsFound = append(carsFound, car)
+		}
+	}
+	return carsFound, nil
+}
+
+func (carRepoMock *CarRepoMock) SelectCountWhere(carFields models.Car) (int, error) {
+	return len(carRepoMock.cars), nil
+}
+
+func (carRepoMock *CarRepoMock) Create(car models.Car) (string, error) {
+	if car.ID == "" {
+		car.ID = uuid.NewString()
+	}
+	carRepoMock.cars = append(carRepoMock.cars, car)
+
+	return car.ID, nil
+}
+
+func (carRepoMock *CarRepoMock) AddToAmtParkingDaysUsed(id string, days int) error {
+	i := util.Find(carRepoMock.cars, func(car models.Car) bool { return car.ID == id })
+	if i == -1 {
+		return errs.NewNotFound("car")
+	}
+	car := &carRepoMock.cars[i]
+
+	*car.AmtParkingDaysUsed = *car.AmtParkingDaysUsed + days
+	return nil
+}
+
+func (carRepoMock *CarRepoMock) Update(carFields models.Car) error {
+	i := util.Find(carRepoMock.cars, func(car models.Car) bool { return car.ID == carFields.ID })
+	if i < 0 {
+		return errs.NewNotFound("car")
+	}
+	car := &carRepoMock.cars[i]
+	if carFields.LicensePlate != "" {
+		car.LicensePlate = carFields.LicensePlate
+	}
+	if carFields.Color != "" {
+		car.Color = carFields.Color
+	}
+	if carFields.Make != "" {
+		car.Make = carFields.Make
+	}
+	if carFields.Model != "" {
+		car.Model = carFields.Model
+	}
+	if carFields.AmtParkingDaysUsed != nil {
+		*car.AmtParkingDaysUsed = *carFields.AmtParkingDaysUsed
+	}
+	return nil
+}
+
+func (carRepoMock *CarRepoMock) Delete(id string) error {
+	i := util.Find(carRepoMock.cars, func(car models.Car) bool { return car.ID == id })
+	if i == -1 {
+		return errs.NewNotFound("car")
+	}
+	cars := carRepoMock.cars
+
+	// replace the element at the index you want to delete with the last element
+	cars[i] = cars[len(cars)-1]
+
+	// re-size slice to remove the last element
+	carRepoMock.cars = cars[:len(cars)-1]
+
+	return nil
+}
