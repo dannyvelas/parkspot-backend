3831f73b16a2cafb436fd7ecc945acd85f6d6932
Daniel Velasquez <danivelas4@gmail.com>
2023-04-29 11:23:07 -0400

    api/hello_handler.go: small/insignificant chang

--- api/hello_handler.go
+++ api/hello_handler.go
@@ -9,12 +9,12 @@ func sayHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
-		AccessPayload, err := ctxGetAccessPayload(ctx)
+		accessPayload, err := ctxGetAccessPayload(ctx)
 		if err != nil {
 			respondError(w, fmt.Errorf("hello_router.sayHello: error getting access payload: %v", err))
 			return
 		}
 
-		respondJSON(w, http.StatusOK, "hello, "+AccessPayload.ID)
+		respondJSON(w, http.StatusOK, "hello, "+accessPayload.ID)
 	}
 }
