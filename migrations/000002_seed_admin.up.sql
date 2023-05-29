4f539c26b609d2f1ded3ff39059344e9fafacc9f
Daniel Velasquez <danivelas4@gmail.com>
2023-05-29 10:52:58 -0400

    migrations/000002_seed_admin.up.sql: added security row

--- migrations/000002_seed_admin.up.sql
+++ migrations/000002_seed_admin.up.sql
@@ -6,10 +6,26 @@ INSERT INTO admin(
   password,
   is_privileged
 ) VALUES(
-  'test',
+  'admin',
   'Daniel',
   'Velasquez',
-  'email@example.com',
+  'admin@example.com',
   '$2a$12$RwfoAooW.NM6Gj6j6BeqC.NpXCfOmdmIzGf3BrmMwfm7bdS5q7yty',
   True
+);
+
+INSERT INTO admin(
+  id,
+  first_name,
+  last_name,
+  email,
+  password,
+  is_privileged
+) VALUES(
+  'security',
+  'Daniel',
+  'Velasquez',
+  'security@example.com',
+  '$2a$12$RwfoAooW.NM6Gj6j6BeqC.NpXCfOmdmIzGf3BrmMwfm7bdS5q7yty',
+  False
 );
